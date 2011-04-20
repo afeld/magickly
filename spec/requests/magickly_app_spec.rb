305d9c5b7ae27704da057d0ebff6db42ada2e83b
Aidan Feldman <aidan@jux.com>
2011-04-20 15:05:14 -0400

    spec refactoring

--- spec/requests/magickly_app_spec.rb
+++ spec/requests/magickly_app_spec.rb
@@ -7,36 +7,43 @@ describe MagicklyApp do
     MagicklyApp
   end
   
-  it "says hello" do
-    get '/'
-    last_response.should be_ok
-  end
-  
-  it "retrieves an image with no options" do
-    image_url = "http://www.foo.com/imagemagick.png"
-    image_path = File.join(File.dirname(__FILE__), '..', 'support', 'imagemagick.png')
-    stub_request(:get, image_url).to_return(:body => File.new(image_path))
+  describe "GET /" do
     
-    get "/?src=#{image_url}"
+    it "should display the welcome page for no params" do
+      get '/'
+      last_response.should be_ok
+      # TODO test that it renders the view
+    end
     
-    a_request(:get, image_url).should have_been_made.once
-    last_response.should be_ok
+    it "should display the welcome page for no src"
     
-    # check that the returned file is identical to the original
-    last_response.body.should eq IO.read(image_path)
-  end
-  
-  it "resizes an image" do
-    image_url = "http://www.foo.com/imagemagick.png"
-    image_path = File.join(File.dirname(__FILE__), '..', 'support', 'imagemagick.png')
-    stub_request(:get, image_url).to_return(:body => File.new(image_path))
-    width = 100
+    it "retrieves an image with no options" do
+      image_url = "http://www.foo.com/imagemagick.png"
+      image_path = File.join(File.dirname(__FILE__), '..', 'support', 'imagemagick.png')
+      stub_request(:get, image_url).to_return(:body => File.new(image_path))
+      
+      get "/?src=#{image_url}"
+      
+      a_request(:get, image_url).should have_been_made.once
+      last_response.should be_ok
+      
+      # check that the returned file is identical to the original
+      last_response.body.should eq IO.read(image_path)
+    end
     
-    get "/?src=#{image_url}&resize=#{width}x"
+    it "resizes an image" do
+      image_url = "http://www.foo.com/imagemagick.png"
+      image_path = File.join(File.dirname(__FILE__), '..', 'support', 'imagemagick.png')
+      stub_request(:get, image_url).to_return(:body => File.new(image_path))
+      width = 100
+      
+      get "/?src=#{image_url}&resize=#{width}x"
+      
+      a_request(:get, image_url).should have_been_made.once
+      last_response.should be_ok
+      
+      ImageSize.new(last_response.body).get_width.should eq width
+    end
     
-    a_request(:get, image_url).should have_been_made.once
-    last_response.should be_ok
-    
-    ImageSize.new(last_response.body).get_width.should eq width
   end
 end
