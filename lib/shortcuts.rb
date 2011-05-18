d6153906e17c23c09d2ea3f023aa1e3a46893fa6
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-18 01:20:31 -0400

    modifying tilt_shift function, making it faster

--- lib/shortcuts.rb
+++ lib/shortcuts.rb
@@ -6,10 +6,11 @@ Magickly.dragonfly.configure do |c|
   c.job :tilt_shift do |coefficients|
     if coefficients == 'true'
       # use default polynomial coefficients
-      coefficients = "2,-2,0.5"
+      coefficients = '4,-4,1'
     end
     
-    action = "\\( +clone -sparse-color Barycentric '0,0 black 0,%[fx:h-1] white' -function polynomial #{coefficients} \\) -compose Blur -set option:compose:args 15 -composite"
+    # note: can be made faster by decreasing blur radius passed to option:compose:args
+    action = "\\( +clone -sparse-color Barycentric '0,0 black 0,%h white' -function polynomial #{coefficients} \\) -compose Blur -set option:compose:args 8 -composite"
     process :convert, action
   end
 end
