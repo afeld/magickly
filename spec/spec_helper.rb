bc3599f2d1df017db7d19240f3b43baba98a7fd7
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-26 08:31:09 -0400

    remove ruby-debug - too much hassle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'rack/test'
 require 'sinatra'
 require 'webmock/rspec'
 require 'image_size'
-require 'ruby-debug'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
